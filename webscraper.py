143553956cc2fa8653c5abf31233c5e08f65874f
Pranavsawant1240 <155555243+Pranavsawant1240@users.noreply.github.com>
2024-07-31 18:46:06 +0530

    webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -1,6 +1,42 @@
 import requests
 from bs4 import BeautifulSoup
+url = "https://codewithharry.com"
 
-req = requests.get("https://dypgroup.edu.in/")
-soup=BeautifulSoup(req.content,"html.parser")
+
+r = requests.get(url)
+htmlContent = r.content
+print(htmlContent)
+
+soup = BeautifulSoup(htmlContent, 'html.parser')
+print(soup.prettify)
+
+title = soup.title
+
+paras = soup.find_all('p')
+print(paras)
+
+print(soup.find('p') ) 
+
+print(soup.find('p')['class'])
+
+print(soup.find_all("p", class_="lead"))
+
+print(soup.find('p').get_text())
 print(soup.get_text())
+
+anchors = soup.find_all('a')
+all_links = set()
+# Get all the links on the page:
+for link in anchors:
+    if(link.get('href') != '#'): 
+        linkText = "https://codewithharry.com" +link.get('href')
+        all_links.add(link)
+        # print(linkText)
+
+
+elem = soup.select('.modal-footer')
+print(elem)
+elem = soup.select('loginModal')
+print(elem)
+
+ 
